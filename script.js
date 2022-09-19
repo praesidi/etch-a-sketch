bd7cbadad45f8cc5773778f66786dafc9aec58ac
Elmir Khalilov <edik.halilov1999@gmail.com>
2022-09-19 18:43:57 +0300

    Make canvas generator with choosen size

--- script.js
+++ script.js
@@ -1,38 +1,66 @@
 /*TODO:
-1. Сделать генерацию полотна по заданному значению (разрешению)
-2. Сделать отображение текушего разрешения над слайдером !DONE!
-3. Добавить функцию для очистки полотна
+*1. Сделать генерацию полотна по заданному значению (разрешению) !DONE!
+*2. Сделать отображение текушего разрешения над слайдером !DONE!
+3. Добавить функцию для очистки полотна ~
 4. Добавить функцию для актививации ластика (очистка полотна "попиксельно")
 5. Добавить функцию для актививации RGB мода
 6. Добавить функцию для рисования заданным цветом
 7. Сделать эффект "нажатой" кнопки при выборе режима
 */
 
-const canvas = document.getElementsByClassName("canvas");
-const slider = document.getElementsByClassName("slider");
+const defaultCanvasSize = 16;
 
-/*function createCanvas(canvasResolution){
-    
+let canvasSize = defaultCanvasSize;
+let canvas = document.getElementById("canvas");
+let color = document.getElementById("colorSelection").value;
+console.log(color);
+
+const clearBtn = document.getElementById ("clearBtn");
+const eraserBtn = document.getElementById ("eraserBtn");
+const rainbowModeBtn = document.getElementById ("rainbowModeBtn");
+const slider = document.getElementById("canvasResolutionRange");
+
+function createCanvas(canvasSize){
+    canvas.style.setProperty('--size', canvasSize);
+    for (let i = 0; i < canvasSize * canvasSize; i++) {
+        const div = document.createElement('div');
+        div.classList.add('pixel');
+        canvas.appendChild(div);
+
+        div.addEventListener('mousedown', function(){
+            div.style.setProperty('background-color', color)
+        });
+    }
+
 }
 
-function paintOnCanvas(){
-
-}
-
-function eraseCanvasElement(){
-
-}
-
-function clearCanvas(){
-
+/*function erasePixel(){
+    color = "#FFFFFF";
 }*/
 
-function showSliderValue(val){
-    document.getElementById("sliderValue").innerHTML = val + " x " + val;
+function clearCanvas(){
+    canvas.innerHTML = '';
+    createCanvas(canvasSize);
 }
 
-/*slider.addEventListener("mouseup", function(){
-    createCanvas(canvasResolution)
-});*/
+function showCanvasResolution(canvasSize){
+    document.getElementById('canvasResolutionValue').innerText = canvasSize + " x " 
+    + canvasSize;
+}
 
-//console.log(canvasResolution);
+
+clearBtn.addEventListener('click', function(){
+    clearCanvas();
+})
+
+slider.addEventListener('input', function(){ //create canvas when we change resolution
+    canvas.innerHTML = '';
+    canvasSize = this.value;
+    showCanvasResolution(canvasSize);
+    createCanvas(canvasSize)
+    
+});
+
+window.onload = () => {
+    createCanvas(defaultCanvasSize);
+}
