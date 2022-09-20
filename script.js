822e4b323cd15d0d29a9490825a8e96b29a54cd8
Elmir Khalilov <edik.halilov1999@gmail.com>
2022-09-20 10:51:19 +0300

    Fix bugs
    Fixed a problem when we couldn't draw with a pressed mouse button

--- script.js
+++ script.js
@@ -5,39 +5,58 @@
 4. Добавить функцию для актививации ластика (очистка полотна "попиксельно")
 5. Добавить функцию для актививации RGB мода
 *6. Добавить функцию для рисования заданным цветом !DONE!
-7. Сделать эффект "нажатой" кнопки при выборе режима
+7. Сделать нормальное масштабирование при изменении размера страницы
 */
 
 const defaultCanvasSize = 16;
 const defaultColor = "#6C8091";
 
 let draw = false;
+document.body.onmousedown = (e) => {
+    draw = true;
+    e.preventDefault(); //fix a problem when we can't draw bcs drag event appears
+}
+document.body.onmouseup = () => draw = false;
+
 let canvasSize = defaultCanvasSize;
-let canvas = document.getElementById("canvas");
 let color = defaultColor;
+let canvas = document.getElementById("canvas");
 const colorPicker = document.getElementById("colorSelection");
 const clearBtn = document.getElementById ("clearBtn");
 const eraserBtn = document.getElementById ("eraserBtn");
-const rainbowModeBtn = document.getElementById ("rainbowModeBtn");
+const rgbBtn = document.getElementById ("rainbowModeBtn");
 const slider = document.getElementById("canvasResolutionRange");
+const modeBtns = {clearBtn, eraserBtn, rgbBtn};
 
 function createCanvas(canvasSize){
     canvas.style.setProperty('--size', canvasSize);
     for (let i = 0; i < canvasSize * canvasSize; i++) {
         const div = document.createElement('div');
         div.classList.add('pixel');
-        canvas.appendChild(div);
-
-        div.addEventListener('mouseover', function(){
-            if (!draw) return;
-            div.style.setProperty('background-color', color);
-        });
+        //canvas.appendChild(div);
 
         div.addEventListener('mousedown', function(){
             div.style.setProperty('background-color', color);
         });
+        div.addEventListener('mouseover', function(){
+            if (!draw) return;
+            div.style.setProperty('background-color', color);
+        });
+        canvas.appendChild(div);
     }
+}
 
+function randomColor(){
+    let randomR = Math.floor(Math.random()*256);
+    let randomG = Math.floor(Math.random()*256);
+    let randomB = Math.floor(Math.random()*256);
+
+    color = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    //console.log(color);
+}
+
+function activateEraser(){
+    color = defaultColor;
 }
 
 function clearCanvas(){
@@ -49,7 +68,6 @@ function showCanvasResolution(canvasSize){
     document.getElementById('canvasResolutionValue').innerText = canvasSize + " x " 
     + canvasSize;
 }
-
 
 clearBtn.addEventListener('click', function(){
     clearCanvas();
@@ -65,14 +83,6 @@ slider.addEventListener('input', function(){ //create canvas when we change reso
     showCanvasResolution(canvasSize);
     createCanvas(canvasSize);
     
-});
-
-window.addEventListener('mousedown', function(){
-    draw = true;
-});
-
-window.addEventListener('mouseup', function(){
-    draw = false;
 });
 
 window.onload = () => {
